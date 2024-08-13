8a98e22938d0bffe9b0d482e67dfd4689535c8ed
Jean Moraes <jean.moraes@t10.digital>
2024-08-13 13:17:44 +0100

    feat: adicionando acessibilidade ao formulário de busca

--- js/formPesquisa.js
+++ js/formPesquisa.js
@@ -2,6 +2,9 @@ const formPesquisa = document.querySelector('.form__pesquisa');
 
 formPesquisa.addEventListener('submit', (evento) => {
   evento.preventDefault();
+
+  const primeiroVideoFiltrado = document.querySelector('.videos__item:not(.escondido) > iframe')
+  primeiroVideoFiltrado?.focus()
 });
 
 const barraDePesquisa = document.querySelector(".pesquisar__input");
